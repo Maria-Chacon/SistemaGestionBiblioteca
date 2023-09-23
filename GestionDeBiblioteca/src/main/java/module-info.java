3e8aafc6cb9c0df8489ffee040585af331321613
Maria-Chacon <mariajchm1312@gmail.com>
2023-09-23 17:42:51 -0600

    commit

---
+++ GestionDeBiblioteca/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.mycompany.gestiondebiblioteca {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    opens com.mycompany.gestiondebiblioteca to javafx.fxml;
+    exports com.mycompany.gestiondebiblioteca;
+    
+}
