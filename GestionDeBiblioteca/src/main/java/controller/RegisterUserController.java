0eee15837803f268c5e532c0915ec54ca40bf5c1
DayanaGamboaM <mongeg1715@gmail.com>
2023-10-09 21:38:13 -0600

    register user administrator

---
+++ GestionDeBiblioteca/src/main/java/controller/RegisterUserController.java
@@ -0,0 +1,76 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+
+/**
+ * FXML Controller class
+ *
+ * @author monge
+ */
+public class RegisterUserController implements Initializable {
+
+    @FXML
+    private Button bntBookLoan;
+    @FXML
+    private Button bntEquipmentLoan;
+    @FXML
+    private Button btnSearchEquipment;
+    @FXML
+    private Button btnClose;
+    @FXML
+    private Button btnRegister;
+    @FXML
+    private TextField textFieldEmail;
+    @FXML
+    private TextField textFieldPassword;
+    @FXML
+    private TextField textFieldIdentification;
+    @FXML
+    private TextField textFieldName;
+    @FXML
+    private TextField textFieldLastName;
+    @FXML
+    private TextField textFieldPhone;
+    @FXML
+    private DatePicker datePickerBirthDay;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void bookLoan(ActionEvent event) {
+    }
+
+    @FXML
+    private void equipmentLoan(ActionEvent event) {
+    }
+
+    @FXML
+    private void searchEquipment(ActionEvent event) {
+    }
+
+    @FXML
+    private void close(ActionEvent event) {
+    }
+
+    @FXML
+    private void ActionRegister(ActionEvent event) {
+    }
+    
+}
