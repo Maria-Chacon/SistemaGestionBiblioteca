793bd62d45ace329ec8db04565446093f4036d37
Maria-Chacon <123408968+Maria-Chacon@users.noreply.github.com>
2023-10-11 15:20:20 -0600

    commit

--- GestionDeBiblioteca/src/main/java/module-info.java
+++ GestionDeBiblioteca/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module com.mycompany.gestiondebiblioteca {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.base;
+    requires java.naming;
+    requires mysql.connector.java;
+    requires java.sql;
 
     opens com.mycompany.gestiondebiblioteca to javafx.fxml;
     exports com.mycompany.gestiondebiblioteca;
