ee164bd83876684f096515dd7128ba23f6f18494
DayanaGamboaM <mongeg1715@gmail.com>
2023-10-11 15:40:14 -0600

    Create AuthorTest.java

---
+++ GestionDeBiblioteca/src/test/java/model/AuthorTest.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+import java.util.Date;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author monge
+ */
+public class AuthorTest {
+    
+    public AuthorTest() {
+    }
+
+    @Test
+    public void testGetBooks() {
+        Book book1 = new Book(false, 5, "Author 1", "Genre 1", "B1", "Book 1");
+        Book book2 = new Book(true, 3, "Author 2", "Genre 2", "B2", "Book 2");
+
+        
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(book1);
+        books.add(book2);
+
+        
+        Author author = new Author(books, new Date(), "123456789", "De Cervantes", "Miguel", "123456789");
+
+        
+        ArrayList<Book> resultBooks = author.getBooks();
+
+        assertEquals(books, resultBooks);
+    }
+    
+}
