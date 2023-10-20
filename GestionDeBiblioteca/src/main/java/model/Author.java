d7c6def2722750ebc7673dfe2e885f62a28bc6bc
DayanaGamboaM <mongeg1715@gmail.com>
2023-10-19 22:18:35 -0600

    Update Author.java

--- GestionDeBiblioteca/src/main/java/model/Author.java
+++ GestionDeBiblioteca/src/main/java/model/Author.java
@@ -11,15 +11,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
 
-
 //Universidad Nacional, Coto
 //Desarrollado por:
 //María José Chacón Mora
 //Dayana Gamboa Monge
 //2023
-
 public class Author extends Person {
-    
+
     private ArrayList<Book> books;
 
     public Author() {
@@ -30,8 +28,6 @@ public class Author extends Person {
         this.books = books;
     }
 
-   
-
     public ArrayList<Book> getBooks() {
         return books;
     }
@@ -39,45 +35,48 @@ public class Author extends Person {
     public void setBooks(ArrayList<Book> books) {
         this.books = books;
     }
-    
+
     public static ArrayList<Author> getAuthorsFromDatabase() {
         ArrayList<Author> authors = new ArrayList<>();
 
-        Conexion connection = Conexion.getInstance(); 
+        Conexion connection = Conexion.getInstance();
 
         try {
-            connection.conectar(); 
-   
+            connection.conectar();
+
             PreparedStatement statement = connection.preparedStatement("SELECT * FROM tbl_authors");
             ResultSet result = statement.executeQuery();
 
-            while (result.next()) {
-                Author author = new Author();
-                author.setId(result.getInt("id"));
-                author.setName(result.getString("name"));
-                author.setLastName(result.getString("lastName"));
-                author.setBirthDay(result.getDate("birthDay"));
-                author.setIdentification(result.getString("identification"));
-                author.setPhone(result.getString("phone"));
-                
-                authors.add(author);
-            }
-        } catch (SQLException ex) {
             
+            getAuthorsRecursive(result, authors);
+        } catch (SQLException ex) {
             System.err.println("Error al obtener autores desde la base de datos: " + ex.getMessage());
         } finally {
-            connection.desconectar(); 
+            connection.desconectar();
         }
 
         return authors;
+    }
+
+    private static void getAuthorsRecursive(ResultSet result, ArrayList<Author> authors) throws SQLException {
+        if (result.next()) {
+            Author author = new Author();
+            author.setId(result.getInt("id"));
+            author.setName(result.getString("name"));
+            author.setLastName(result.getString("lastName"));
+            author.setBirthDay(result.getDate("birthDay"));
+            author.setIdentification(result.getString("identification"));
+            author.setPhone(result.getString("phone"));
+
+            authors.add(author);
+
+            getAuthorsRecursive(result, authors);
+        }
     }
 
     @Override
     public String toString() {
         return getName() + " " + getLastName();
     }
-    
-    
-    
-    
+
 }
